6e4edfedcf8eb979ab052acab5b3b42abca68714
tosKenil <kenil.dev@theoceanstudios.in>
2025-09-24 15:36:38 +0530

    tt

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 const { RtcTokenBuilder, RtcRole } = require("agora-access-token");
 const Tesseract = require("tesseract.js");
 const multer = require("multer");
